2f9ea107fb90472cff7276d832015b54d42111d8
khasenovweb <hasenovkirill93@gmail.com>
2020-06-15 13:58:03 +0500

    sdfhsdkjh

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+$(".slider").owlCarousel({
+  items: 1,
+  loop: true,
+  nav: true,
+  navText: [
+    '<img src="/img/arrow__left.png">',
+    '<img src="/img/arrow__right.png" >',
+  ],
+});
+
+$(".clients__slider").owlCarousel({
+  items: 6,
+  loop: true,
+  margin: 50,
+  nav: true,
+  navText: [
+    '<img src="/img/arrow__left.png">',
+    '<img src="/img/arrow__right.png" >',
+  ],
+});
